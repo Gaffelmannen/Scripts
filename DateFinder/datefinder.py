14130c559f3c4d483998c10969bb1e7b885d86a8
Hans Sandström <hans.sandstrom@gmail.com>
2019-12-02 08:55:35 +0100

    Added datefinder script.

---
+++ DateFinder/datefinder.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys
+from datetime import date
+from dateutil.relativedelta import relativedelta
+
+class DateFinder:
+    def __init__(self):
+        #pass
+        self.start_date = date.today()
+        self.number_of_months = 3
+    
+    def get_start_date(self):
+        return self.start_date
+        
+    def get_offset(self):
+        return self.number_of_months
+
+    def calculate_day(self):
+        calculated_date =  \
+            self.get_start_date() + \
+            relativedelta(months=+self.get_offset())
+        return calculated_date
+
+if __name__ == "__main__":
+    df = DateFinder()
+    print(df.calculate_day())
+    sys.exit(0)
