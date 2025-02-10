9f8fee3f98889976b38c8f625a6f848e35544348
Hans <hans.sandstrom@gmail.com>
2025-02-10 15:52:34 +0100

    Added menu to datefinder.

--- DateFinder/datefinder.py
+++ DateFinder/datefinder.py
@@ -4,27 +4,58 @@
 import sys
 from datetime import date
 from dateutil.relativedelta import relativedelta
+from colorama import Fore, Back, Style
 
-class DateFinder:
-    def __init__(self):
-        #pass
-        #self.start_date = date.today()
-        self.start_date = date(2023, 3, 1)
-        self.number_of_months = 6
-    
-    def get_start_date(self):
-        return self.start_date
-        
-    def get_offset(self):
-        return self.number_of_months
-
-    def calculate_day(self):
-        calculated_date =  \
-            self.get_start_date() + \
-            relativedelta(months=+self.get_offset())
-        return calculated_date
+import simple_term_menu as stm
 
 if __name__ == "__main__":
-    df = DateFinder()
-    print(df.calculate_day())
+
+    title = "Datefinder 1.0"
+    mapping = \
+    { \
+        "One month" : "one-month", \
+        "Three months" : "three-months", \
+        "Six months" : "six-months", \
+        "Quit" : "Quit"
+    }
+    info = "Please choose your request below:"
+    caption = "Welcome to Datefinder."
+    main_menu_cursor = "> "
+    main_menu_cursor_style = ("fg_red", "bold")
+    main_menu_style = ("bg_red", "fg_yellow")
+
+    print(Fore.CYAN + title + Style.RESET_ALL)
+
+    while True:
+
+        print(Fore.GREEN + caption + Style.RESET_ALL)
+        
+        terminal_menu = stm.TerminalMenu \
+        (
+            mapping.keys(), 
+            title=info,
+            menu_cursor=main_menu_cursor,
+            menu_cursor_style=main_menu_cursor_style,
+            menu_highlight_style=main_menu_style
+        )
+        
+        selected = terminal_menu.show()
+
+        print("Selected: {}".format(list(mapping.keys())[selected]))
+
+        if list(mapping.keys())[selected] == "Quit":
+            print("Thanks for using Datefinder 1.0")
+            break
+
+        choice = list(mapping.values())[selected]
+        result = \
+        {
+            "one-month": lambda result: date.today() + relativedelta(months=+1),
+            "three-months": lambda result: date.today() + relativedelta(months=+3),
+            "six-months": lambda result: date.today() + relativedelta(months=+6)
+        }["one-month"](choice)
+        
+        print("The calculated date is: {}".format(result))
+        print("")
+    
     sys.exit(0)
